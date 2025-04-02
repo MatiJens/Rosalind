f5bd5019d83bc9658a7c8b6b52f1b230057c5d4d
MatiJens <mateuszjens14@gmail.com>
2025-04-02 10:28:51 +0200

    01_DNA final

--- Bioinformatics Stronghold/p01_DNA.py
+++ Bioinformatics Stronghold/p01_DNA.py
@@ -1,21 +1,18 @@
-def countNucleotide(DNA):
+with open("txt/rosalind_dna.txt", "r") as f:
+    DNA = f.read()
     if isinstance(DNA, str):
         nucleotides = [0, 0, 0, 0]
         for nucleotide in DNA:
             if nucleotide == 'A':
                 nucleotides[0] += 1
-                print("A")
             elif nucleotide == 'C':
                 nucleotides[1] += 1
-                print("C")
             elif nucleotide == 'G':
                 nucleotides[2] += 1
-                print("G")
             elif nucleotide == 'T':
                 nucleotides[3] += 1
-                print("T")
             else:
                 continue
-        print(nucleotides[0] + " " + nucleotides[1] + " " + nucleotides[2] + " " + nucleotides[3])
+        print(" ".join(map(str, nucleotides)))
     else:
         TypeError("Argument should be str type")
